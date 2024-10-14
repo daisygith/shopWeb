3f2d66cf986e373bf5a6ca0512339c88e3616467
daisygith <daisygith@users.noreply.github.com>
2024-10-14 18:03:32 +0200

    created implementation userService

--- src/main/java/sdu/shopWeb/service/UserServiceImpl.java
+++ src/main/java/sdu/shopWeb/service/UserServiceImpl.java
@@ -2,9 +2,14 @@ package sdu.shopWeb.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import sdu.shopWeb.dto.CartDTO;
 import sdu.shopWeb.dto.UserDTO;
+import sdu.shopWeb.entity.Cart;
+import sdu.shopWeb.entity.User;
 import sdu.shopWeb.repository.UserRepository;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -15,21 +20,68 @@ public class UserServiceImpl implements UserService{
 
     @Override
     public List<UserDTO> findAllUsers() {
-        return null;
+        List<User> userList = userRepository.findAll();
+
+        List<UserDTO> userListDTO = new ArrayList<>();
+
+        userList.forEach((User item) -> {
+            userListDTO.add(UserDTO.builder()
+                            .id(item.getId())
+                            .name(item.getName())
+                    .build());
+        });
+        return userListDTO;
     }
 
     @Override
-    public UserDTO findById(Long theId) throws Exception {
-        return null;
+    public UserDTO findById(Long userId) throws Exception {
+
+        User user = userRepository.findById(userId).orElseThrow();
+
+        UserDTO userDTO = UserDTO.builder()
+                .id(user.getId())
+                .name(user.getName())
+                .cartDTO(CartDTO.builder()
+                        .id(user.getCart().getId())
+                        .sumPrice(user.getCart().getSumPrice())
+                        .build())
+                .build();
+
+
+        return userDTO;
     }
 
+    @Transactional
     @Override
-    public UserDTO save(UserDTO theUserDTO) {
-        return null;
+    public UserDTO save(UserDTO userDTO) {
+
+        User user = userRepository.save(User.builder()
+                        .id(userDTO.getId())
+                        .name(userDTO.getName())
+                        .cart(Cart.builder()
+                                .id(userDTO.getCartDTO().getId())
+                                .sumPrice(userDTO.getCartDTO().getSumPrice())
+                                .build())
+                .build());
+
+        UserDTO userListDTO = UserDTO.builder()
+                .id(user.getId())
+                .name(user.getName())
+                .build();
+
+        if(user.getCart() != null){
+            userListDTO.setCartDTO(CartDTO.builder()
+                            .id(userDTO.getCartDTO().getId())
+                            .sumPrice(userDTO.getCartDTO().getSumPrice())
+                    .build());
+        }
+
+        return userListDTO;
     }
 
+    @Transactional
     @Override
-    public void deleteById(Long theId) {
-
+    public void deleteById(Long userId) {
+        userRepository.deleteById(userId);
     }
 }
