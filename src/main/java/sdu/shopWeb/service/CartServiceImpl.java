a929ad63903ad918ff2bd03d5e3a0ab2dbe3dcb2
daisygith <daisygith@users.noreply.github.com>
2024-10-14 13:33:30 +0200

    created implementation to CartService

--- src/main/java/sdu/shopWeb/service/CartServiceImpl.java
+++ src/main/java/sdu/shopWeb/service/CartServiceImpl.java
@@ -2,10 +2,18 @@ package sdu.shopWeb.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import sdu.shopWeb.dto.CartDTO;
+import sdu.shopWeb.dto.ProductDTO;
+import sdu.shopWeb.dto.UserDTO;
+import sdu.shopWeb.entity.Cart;
+import sdu.shopWeb.entity.Product;
+import sdu.shopWeb.entity.User;
 import sdu.shopWeb.repository.CartRepository;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class CartServiceImpl implements CartService{
@@ -15,21 +23,91 @@ public class CartServiceImpl implements CartService{
 
     @Override
     public List<CartDTO> findAllCarts() {
-        return null;
+        List<Cart> cartList = cartRepository.findAll();
+
+        List<CartDTO> cartListDTO = new ArrayList<>();
+
+        cartList.forEach((Cart item) -> {
+            cartListDTO.add(CartDTO.builder()
+                            .id(item.getId())
+                            .sumPrice(item.getSumPrice())
+                    .build());
+        });
+
+        return cartListDTO;
+
     }
 
     @Override
-    public CartDTO findById(Long theId) throws Exception {
-        return null;
+    public CartDTO findById(Long cartId) throws Exception {
+
+        Cart cart = cartRepository.findById(cartId).orElseThrow();
+
+        CartDTO cartDTO = CartDTO.builder()
+                .id(cart.getId())
+                .sumPrice(cart.getSumPrice())
+                .userDTO(UserDTO.builder()
+                        .id(cart.getUser().getId())
+                        .name(cart.getUser().getName())
+                        .build())
+                .productsDTO(cart.getProducts().stream()
+                        .map(x -> ProductDTO.builder()
+                                .id(x.getId())
+                                .name(x.getName())
+                                .price(x.getPrice())
+                                .build()).collect(Collectors.toList()))
+                .build();
+
+        return cartDTO;
     }
 
+    @Transactional
     @Override
     public CartDTO save(CartDTO cartDTO) {
-        return null;
+
+        Cart cart = cartRepository.save(Cart.builder()
+                        .id(cartDTO.getId())
+                        .sumPrice(cartDTO.getSumPrice())
+                        .user(User.builder()
+                                .id(cartDTO.getUserDTO().getId())
+                                .name(cartDTO.getUserDTO().getName())
+                                .build())
+                        .products(cartDTO.getProductsDTO() != null ?
+                                cartDTO.getProductsDTO().stream()
+                                        .map(x -> Product.builder()
+                                                .id(x.getId())
+                                                .name(x.getName())
+                                                .price(x.getPrice())
+                                                .build()).collect(Collectors.toList()): null)
+                .build());
+
+        CartDTO cartListDTO = CartDTO.builder()
+                .id(cart.getId())
+                .sumPrice(cart.getSumPrice())
+                .build();
+
+        if(cart.getUser() != null) {
+            cartListDTO.setUserDTO(UserDTO.builder()
+                    .id(cartDTO.getUserDTO().getId())
+                    .name(cartDTO.getUserDTO().getName())
+                    .build());
+        }
+
+        if(cart.getProducts() != null) {
+            cartListDTO.setProductsDTO(cartDTO.getProductsDTO().stream()
+                    .map(x-> ProductDTO.builder()
+                            .id(x.getId())
+                            .name(x.getName())
+                            .price(x.getPrice())
+                            .build()).collect(Collectors.toList()));
+        }
+
+        return cartListDTO;
     }
 
+    @Transactional
     @Override
-    public void deleteById(Long theId) {
-
+    public void deleteById(Long cartId) {
+        cartRepository.deleteById(cartId);
     }
 }
