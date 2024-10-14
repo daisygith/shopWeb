ca4dc3d09937f221c40c0a33c69b85533902523d
daisygith <daisygith@users.noreply.github.com>
2024-10-14 13:52:03 +0200

    created implementation for productService

--- src/main/java/sdu/shopWeb/service/ProductServiceImpl.java
+++ src/main/java/sdu/shopWeb/service/ProductServiceImpl.java
@@ -2,9 +2,12 @@ package sdu.shopWeb.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import sdu.shopWeb.dto.ProductDTO;
+import sdu.shopWeb.entity.Product;
 import sdu.shopWeb.repository.ProductRepository;
 
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -16,21 +19,57 @@ public class ProductServiceImpl implements ProductService{
 
     @Override
     public List<ProductDTO> findAllProducts() {
-        return null;
+        List<Product> productList = productRepository.findAll();
+
+        List<ProductDTO> productListDTO = new ArrayList<>();
+
+        productList.forEach((Product item) -> {
+            productListDTO.add(ProductDTO.builder()
+                            .id(item.getId())
+                            .name(item.getName())
+                            .price(item.getPrice())
+                    .build());
+        });
+
+        return productListDTO;
     }
 
     @Override
-    public ProductDTO findById(Long theId) throws Exception {
-        return null;
+    public ProductDTO findById(Long productId) throws Exception {
+
+        Product product = productRepository.findById(productId).orElseThrow();
+
+        ProductDTO productDTO = ProductDTO.builder()
+                .id(product.getId())
+                .name(product.getName())
+                .price(product.getPrice())
+                .build();
+
+        return productDTO;
     }
 
+    @Transactional
     @Override
     public ProductDTO save(ProductDTO productDTO) {
-        return null;
+
+        Product product = productRepository.save(Product.builder()
+                        .id(productDTO.getId())
+                        .name(productDTO.getName())
+                        .price(productDTO.getPrice())
+                .build());
+
+        ProductDTO productListDTO = ProductDTO.builder()
+                .id(product.getId())
+                .name(product.getName())
+                .price(product.getPrice())
+                .build();
+
+        return productListDTO;
     }
 
+    @Transactional
     @Override
-    public void deleteById(Long theId) {
-
+    public void deleteById(Long productId) {
+        productRepository.deleteById(productId);
     }
 }
